95a59de9cb99547729d48d0e9f19c47b916a0f09
samohik <invasio12@mail.ru>
2023-02-08 17:32:20 +0300

    v1.0

--- main.py
+++ main.py
@@ -1,60 +1,144 @@
+import json
+import os
 import time
 import uuid
+from typing import Tuple, Dict, List, Any
+
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-
-# unique_id = uuid.uuid4()
-# 👉️ 011963c3-7fa3-4963-8599-a302f9aefe7d
-
-
-def test():
-    driver = webdriver.Chrome()
-    url = 'https://www.truckscout24.com/vans/used/refrigerated-truck/renault'
-    driver.get(url)
-    time.sleep(3)
-    element = driver.find_element(By.CLASS_NAME, 'ls-arrow')
-    element.click()
-    time.sleep(3)
-    html = driver.page_source
-    driver.close()
-    return html
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Parser:
-    def __init__(self):
-        self.url = 'https://www.truckscout24.ru/transporter/gebraucht/kuehl-iso-frischdienst/renault'
+    def __init__(self, url):
+        self.driver = webdriver.Chrome()
+        self.url = url
+        self.l_links = []
+        self.data = {"ads": []}
 
-    def get_response(self):
-        response = requests.get(self.url).content
-        return response
+    def get_html(self, url) -> str:
+        """
+        Gets full html with details after click on products.
+        :return: str
+        """
+        self.driver.get(url)
+        items = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, '//div[@class="ls-elem ls-elem-gap"]')
+            )
+        )
+        for index, _ in enumerate(items):
+            element = WebDriverWait(self.driver, 1).until(
+                EC.element_to_be_clickable(
+                    (By.XPATH, f'//div[@data-container-id="{index}"]')
+                )
+            )
+            action = ActionChains(self.driver)
+            action.move_to_element(element).perform()
+            self.driver.execute_script("arguments[0].click();", element)
+        time.sleep(0.2)
+        html = self.driver.page_source
+        return html
 
-    def scraping(self, content):
-        soup = BeautifulSoup(content, 'lxml')
-        items = soup.find_all('div', class_='ls-elem ls-elem-gap')
+    def get_pages(self) -> None:
+        """
+        Finds links to other pages.
+        :return: None
+        """
+        self.driver.get(self.url)
+        content = self.driver.page_source
+        soup = BeautifulSoup(content, "lxml")
+        links = soup.find("ul", class_="sc-pagination")
+        links.findChildren("a", recursive=True)
+        for link in links:
+            link = link.find("a").get("href")
+            if link and link not in self.l_links:
+                self.l_links.append(link)
+
+    def get_detail(self, item) -> Tuple[str, str]:
+        """
+        :return: color and power information
+        """
+        li_s = item.find("div", class_="detail-container").find_all("li")
+        color: str | None = None
+        power = None
+        for elem in li_s:
+            if color and power:
+                break
+            else:
+                if elem.div.text == "Colour":
+                    color = elem.div.findNext("div").text.strip()
+                if elem.div.text == "Power":
+                    power = elem.div.findNext("div").text.split(" (")[0]
+        return color, power
+
+    def scraping(self, content: str) -> None:
+        """
+        Getting all information from html.
+        :param content: html
+        :return: Dict
+        """
+        soup = BeautifulSoup(content, "lxml")
+        items = soup.find_all("div", class_="ls-elem ls-elem-gap")
+
         for item in items:
-            title = item.find('h2', class_='ls-makemodel sc-font-bold sc-ellipsis').text
-            price = item.find('div', class_='ls-data-price').text
-            mileage = item.find('div', class_='ls-data-additional').div.text
+            try:
+                description: str = item.find("div", class_="short-description").text
+            except AttributeError:
+                print("If that's too fast, just add the time on line 33.")
+                description = "Nothing!"
+            href: str = item.find("a", {"data-item-name": "detail-page-link"})["href"]
+            title: str = item.find(
+                "h2", class_="ls-makemodel sc-font-bold sc-ellipsis"
+            ).text.strip()
+            price = (
+                item.find("div", class_="ls-data-price").text.strip().replace("\n", "")
+            )
+            try:
+                mileage = item.find("div", class_="ls-data-additional").div.text.strip()
+            except AttributeError:
+                mileage = "Nope"
+            color, power = self.get_detail(item)
+            self.data["ads"].append(
+                {
+                    "id": str(uuid.uuid4()),
+                    "href": href,
+                    "title": title,
+                    "price": price,
+                    "mileage": mileage,
+                    "color": color,
+                    "power": power,
+                    "description": description,
+                }
+            )
 
-            # color = item.find('div', class_='detail-container')
-            # hidden
-            detail_info = item.find_all('li')
-            """class_='sc-font-bold'"""
-            for val in detail_info:
-                print(val)
-                if 'Colour' == val.string:
-                    print(val.previous_sibling.string)
-                    print(val.next_sibling.string)
-            # print(title, price, mileage)
-            break
+    @classmethod
+    def file_write(cls, data: Dict[str, List[Any]]) -> None:
+        """
+        Writing all information to data/data.json.
+        :param data: product information
+        :return: None
+        """
+        os.mkdir("data")
+        with open("data/data.json", "w") as file:
+            json_data = json.dumps(data)
+            file.write(json_data)
 
-    def main(self):
-        self.scraping(content=test())
+    def main(self) -> None:
+        """
+        Executing function.
+        :return: None
+        """
+        self.l_links.append(self.url)
+        self.get_pages()
+        for url in self.l_links:
+            self.scraping(content=self.get_html(url))
+        self.file_write(self.data)
 
 
-if __name__ == '__main__':
-    # test()
-    a = Parser()
+if __name__ == "__main__":
+    a = Parser("https://www.truckscout24.com/vans/used/refrigerated-truck/renault")
     a.main()
